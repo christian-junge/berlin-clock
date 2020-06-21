501e8e35f1f7e0c9eb2c9454ecd11fab4f480062
Christian Junge <christianjunge@arcor.de>
2020-06-21 19:46:03 +0200

    Implementation of TimeParser class

--- BerlinClockService/TimeParser.cs
+++ BerlinClockService/TimeParser.cs
@@ -8,7 +8,29 @@ namespace BerlinClock.Service
         private const string pattern = @"^(\d{2}):(\d{2}):(\d{2}$)";
         
         public TimeSpan ParseTimeString(string input) {
-            throw new NotImplementedException();
+            if (IsValid(input)) {
+                return TimeSpan.Parse(input);
+            } else {
+                throw new FormatException("Incorrect time format (hh:mm:ss)");
+            }
+        }
+
+        private bool IsValid(string input) {
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            if (matches.Count != 1) {
+                return false;
+            }
+
+            int hours = Int32.Parse(matches[0].Groups[1].Value);
+            int minutes = Int32.Parse(matches[0].Groups[2].Value);
+            int seconds = Int32.Parse(matches[0].Groups[3].Value);
+
+            if (hours > 23 || minutes > 60 || seconds > 60) {
+                return false;
+            }
+
+            return true;
         }
     }
 }
