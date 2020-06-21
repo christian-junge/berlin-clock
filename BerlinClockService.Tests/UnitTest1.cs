041cdc533c066504a57632182740f713e2b8e913
Christian Junge <christianjunge@arcor.de>
2020-06-21 19:05:20 +0200

    Initial commit

---
+++ BerlinClockService.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BerlinClockService.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
