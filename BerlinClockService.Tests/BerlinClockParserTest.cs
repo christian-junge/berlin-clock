7afb755c1e56ba4f683d83b95df0f274f90039eb
Christian Junge <christianjunge@arcor.de>
2020-06-21 20:17:29 +0200

    BerlinClockParser test case data

--- BerlinClockService.Tests/BerlinClockParserTest.cs
+++ BerlinClockService.Tests/BerlinClockParserTest.cs
@@ -8,7 +8,11 @@ namespace BerlinClock.Tests
     public class BerlinClockParserTest
     {
         [Theory]
-        [InlineData("13:17:02", "xxx")]
+        [InlineData("13:17:02", "Y\nRROO\nRRRO\nYOOOOOOOOOO\nYYOO")]
+        [InlineData("02:10:17", "O\nOOOO\nRROO\nYYOOOOOOOOO\nOOOO")]
+        [InlineData("23:59:02", "Y\nRRRR\nRRRO\nYYRYYRYYRYY\nYYYY")]
+        [InlineData("00:00:33", "O\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO")]
+        [InlineData("16:34:59", "O\nRRRO\nROOO\nYYRYYROOOOO\nYYYY")]
         public void ShouldReturnString(string input, string output)
         {
             Mock<ITimeParser> timeParserMock = new Mock<ITimeParser>();
